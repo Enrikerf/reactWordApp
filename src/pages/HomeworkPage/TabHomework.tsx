25a414df0d1541c365a779a33bed7cfdb2a22694
Enrique Arrabal <enrike9066@gmail.com>
2020-05-23 13:13:20 +0200

    Basic style on homework

--- src/pages/HomeworkPage/TabHomework.tsx
+++ src/pages/HomeworkPage/TabHomework.tsx
@@ -8,73 +8,106 @@ import {
   IonLabel,
   IonItem,
   IonInput,
-  IonCard,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonCardContent,
   IonIcon,
   IonList,
-  IonListHeader,
   IonItemOption,
   IonItemSliding,
   IonItemOptions,
+  IonText,
 } from "@ionic/react";
 import { volumeMediumOutline } from "ionicons/icons";
 import { Entry } from "../../shared/modules/api-kerf/models/entry";
 
-const TabHomework: React.FC = () => {
-  const styles = {
-    volumeIcon: {
-      display: "flex",
-      justifyContent: "flex-end",
-    },
-  };
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>homework</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+class TabHomework extends React.Component {
+  public entries: Entry[];
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+    this.entries = [
+      {
+        word: "string",
+        results: [],
+        pronunciation: "any",
+        frequency: 4,
+      },
+      {
+        word: "string",
+        results: [],
+        pronunciation: "any",
+        frequency: 4,
+      },
+      {
+        word: "string",
+        results: [],
+        pronunciation: "any",
+        frequency: 4,
+      },
+    ];
+  }
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>homework</IonTitle>
+          </IonToolbar>
+        </IonHeader>
 
-      <IonContent>
-        <form>
-          <div className="form-group">
-            <IonItem>
-              <IonLabel position="floating">Buscar</IonLabel>
-              <IonInput
-                id="wordToSearch"
-                name="wordToSearch"
-                class="form-control"
-              ></IonInput>
-            </IonItem>
-          </div>
-        </form>
-        <div className="list-content">
-          <IonList>
-            <IonItemSliding>
+        <IonContent>
+          <form>
+            <div className="form-group">
               <IonItem>
-                <IonLabel>Set</IonLabel>
-                <IonLabel>set</IonLabel>
-                <IonLabel>range: 1K</IonLabel>
-                <div style={styles.volumeIcon}>
-                  <IonIcon size="large" icon={volumeMediumOutline} />
-                </div>
+                <IonLabel position="floating">Buscar</IonLabel>
+                <IonInput
+                  id="wordToSearch"
+                  name="wordToSearch"
+                  class="form-control"
+                ></IonInput>
               </IonItem>
+            </div>
+          </form>
+          <div>
+            <IonList>
+              {this.entries.map((el) => (
+                <EntryCard />
+              ))}
+            </IonList>
+          </div>
+        </IonContent>
+      </IonPage>
+    );
+  }
+}
 
-              <IonItemOptions side="start">
-                <IonItemOption color="primary">Mark Unread</IonItemOption>
-              </IonItemOptions>
-
-              <IonItemOptions side="end">
-                <IonItemOption color="danger">Delete</IonItemOption>
-              </IonItemOptions>
-            </IonItemSliding>
-          </IonList>
-        </div>
-      </IonContent>
-    </IonPage>
-  );
-};
+class EntryCard extends React.Component {
+  render() {
+    const styles = {
+      none: {},
+    };
+    return (
+      <IonItemSliding style={styles.none}>
+        <IonItem lines="none" style={styles.none}>
+          <IonLabel class="ion-text-wrap" onClick={() => alert("item")}>
+            <IonText color="primary">
+              <h3>H3 Primary Title</h3>
+            </IonText>
+            <p>Paragraph line 1</p>
+          </IonLabel>
+          <IonIcon
+            size="large"
+            slot="end"
+            icon={volumeMediumOutline}
+            onClick={() => alert("volumen")}
+          />
+        </IonItem>
+        <IonItemOptions side="start">
+          <IonItemOption color="success">Done</IonItemOption>
+        </IonItemOptions>
+        <IonItemOptions side="end">
+          <IonItemOption color="secondary">Remind</IonItemOption>
+        </IonItemOptions>
+      </IonItemSliding>
+    );
+  }
+}
 export default TabHomework;
